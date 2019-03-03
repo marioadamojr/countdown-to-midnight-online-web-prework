9dbc7c3dc1836c675c44ca969226a8b5adb97d3f
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 00:02:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,15 @@ def countdown(number)
   end
   hny
 end
+
+
+#write your code here
+
+def countdown_with_sleep(number)
+  hny = "HAPPY NEW YEAR!"
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    number -= 2
+  end
+  hny
+end
