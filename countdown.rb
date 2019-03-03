ce8469e9bf85fd618ecc08efdd3e811062de3d00
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 00:03:22 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  hny = "HAPPY NEW YEAR!"
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  hny
+end
+
+
+#write your code here
+
+def countdown_with_sleep(number)
+  sleep(5)
+  hny = "HAPPY NEW YEAR!"
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  hny
 end
